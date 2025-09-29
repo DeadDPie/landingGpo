64b28e90e799a62310b2eacfe8929793ee7d68e9
DeadDPie <elizabethfox434@gmail.com>
2025-09-29 13:00:13 +0700

    add order button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Catalog from "@/components/Catalog/Catalog";
 import Header from "@/components/Header/Header";
+import Order from "@/components/Order/Order";
 
 export default function Home() {
   return (
@@ -7,7 +8,9 @@ export default function Home() {
       <main>
         <Header />
         <Catalog />
+
         <p>Здесь будем вставлять наши блоки лендинга</p>
+        <Order />
       </main>
     </div>
   );
