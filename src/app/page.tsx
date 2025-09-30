d052c470649fd6be5ab200cfb263d0811d4b4b85
Anya <annasadkova20479932@gmail.com>
2025-10-01 02:04:12 +0700

    add hero block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Catalog from "@/components/Catalog/Catalog";
 import Header from "@/components/Header/Header";
 import Order from "@/components/Order/Order";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Header />
+        <Hero />
         <Catalog />
         <Order />
       </main>
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,45 @@
+import Typography from "@/ui/Typography/Typography";
+import Image from "next/image";
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.hero_container}>
+        {/* Левая колонка */}
+        <div className={styles.hero_content}>
+          <Typography variant="h1" className={styles.hero_title}>
+            Вкусные торты
+            <br />
+            на заказ!
+          </Typography>
+
+          <div className={styles.hero_images}>
+            <Image
+              src="/images/hero1.png"
+              alt="hero1"
+              width={239}
+              height={288}
+              className={styles.hero_image}
+            />
+            <Typography variant="p1" className={styles.hero_description}>
+              Авторские торты, капкейки и пирожные с индивидуальным дизайном и
+              неповторимым вкусом для ваших праздников
+            </Typography>
+          </div>
+        </div>
+
+        {/* Правая колонка */}
+        <div className={styles.hero_main_image}>
+          <Image
+            src="/images/hero2.png"
+            alt="hero2"
+            width={458}
+            height={552}
+            className={styles.hero_image}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
