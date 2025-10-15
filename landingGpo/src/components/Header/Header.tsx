cde75be7a32e5d382cabbf9727fa736999fa6614
Anya <annasadkova20479932@gmail.com>
2025-10-15 12:25:13 +0700

    Header adaptive

---
+++ landingGpo/src/components/Header/Header.tsx
@@ -0,0 +1,50 @@
+import {socialLinks} from "@/constants/data";
+import Image from "next/image";
+import Typography from "@/ui/Typography/Typography";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <Typography variant="h3" as="a" href="#catalog" className={styles.link}>
+            Каталог
+          </Typography>
+          <Typography variant="h3" as="a" href="#about" className={styles.link}>
+            Обо мне
+          </Typography>
+          <Typography variant="h3" as="a" href="#reviews" className={styles.link}>
+            Отзывы
+          </Typography>
+        </nav>
+
+        <div className={styles.icons}>
+          <a href="#cart" className={styles.cart_link}>
+            <Image
+              src="/icons/cart.svg"
+              alt="cart"
+              width={49}
+              height={48}
+              className={styles.icon}
+            />
+          </a>
+
+          <div className={styles.social_icons}>
+            {socialLinks.map((social) => (
+              <a key={social.id} href={social.href} target="_blank">
+                <Image
+                  src={social.iconHeader}
+                  alt={social.id}
+                  width={64}
+                  height={64}
+                  className={styles.icon}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
